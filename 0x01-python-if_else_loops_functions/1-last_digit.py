045d6943234bbe4e6abb881c0613b41199a47ad5
Iam Ian <74612008+IANDE-X@users.noreply.github.com>
2022-09-20 21:48:50 +0200

    Update 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-str1 = " and is greater than 5"
-str2 = " and is 0"
-str3 = " and is less than 6 and not 0"
-if number < 0:
-    last = number % -10
-else:
-    last = number % 10
-if last > 5:
-    print("Last digit of {} is {}".format(number, last) + str1)
-elif last == 0:
-    print("Last digit of {} is {}".format(number, last) + str2)
-else:
-    print("Last digit of {} is {}".format(number, last) + str3)
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, lastNum;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastNum = n % 10;
+	if (lastNum > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastNum);
+	else if (lastNum == 0)
+		printf("Last digit of %d is %d and is 0\n", n, lastNum);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNum);
+	return (0);
+}
