724905b6c8e87573a6f624f837c390aa5173b027
Iam Ian <74612008+IANDE-X@users.noreply.github.com>
2022-09-22 20:07:03 +0200

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
-if __name__ == "__main__":
-    result = 0
-    if len(argv) == 1:
-        print("0")
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+argc = len(sys.argv) - 1
+
+i = 0
+result = 0
+for arg in sys.argv:
+    if i != 0:
+        result += int(arg)
     else:
-        for i in range(1, len(argv)):
-            result += int(argv[i])
-        print(result)
+        i += 1
+print("{:d}".format(result))
