ee010fe6003d5c1b5d13b3adc5596105e28be0c7
The Ian <74612008+IANDE-X@users.noreply.github.com>
2022-09-27 21:56:26 +0200

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,114 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * reverse - reverses the second half of the list
+ * is_palindrome - Check if a linked list is a palindrome
+ * @head: The list
  *
- * @h_r: head of the second half
- * Return: no return
- */
-void reverse(listint_t **h_r)
-{
-    listint_t *prv;
-    listint_t *crr;
-    listint_t *nxt;
-
-    prv = NULL;
-    crr = *h_r;
-
-    while (crr != NULL)
-    {
-        nxt = crr->next;
-        crr->next = prv;
-        prv = crr;
-        crr = nxt;
-    }
-
-    *h_r = prv;
-}
-
-/**
- * compare - compares each int of the list
- *
- * @h1: head of the first half
- * @h2: head of the second half
- * Return: 1 if are equals, 0 if not
- */
-int compare(listint_t *h1, listint_t *h2)
-{
-    listint_t *tmp1;
-    listint_t *tmp2;
-
-    tmp1 = h1;
-    tmp2 = h2;
-
-    while (tmp1 != NULL && tmp2 != NULL)
-    {
-        if (tmp1->n == tmp2->n)
-        {
-            tmp1 = tmp1->next;
-            tmp2 = tmp2->next;
-        }
-        else
-        {
-            return (0);
-        }
-    }
-
-    if (tmp1 == NULL && tmp2 == NULL)
-    {
-        return (1);
-    }
-
-    return (0);
-}
-
-/**
- * is_palindrome - checks if a singly linked list
- * is a palindrome
- * @head: pointer to head of list
- * Return: 0 if it is not a palindrome,
- * 1 if it is a palndrome
+ * Return: 1 if it's a palindrome, 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow, *fast, *prev_slow;
-    listint_t *scn_half, *middle;
-    int isp;
+	listint_t *a = *head;
+	listint_t *b = *head;
 
-    slow = fast = prev_slow = *head;
-    middle = NULL;
-    isp = 1;
+	if (*head == NULL)
+		return (1);
 
-    if (*head != NULL && (*head)->next != NULL)
-    {
-        while (fast != NULL && fast->next != NULL)
-        {
-            fast = fast->next->next;
-            prev_slow = slow;
-            slow = slow->next;
-        }
+	while (b && b->next && b->next->next)
+	{
+		a = a->next;
+		b = b->next->next;
+	}
 
-        if (fast != NULL)
-        {
-            middle = slow;
-            slow = slow->next;
-        }
+	a = reverse_list(&a);
+	b = *head;
+	while (a && b)
+	{
+		if (a->n != b->n)
+			return (0);
+		a = a->next;
+		b = b->next;
+	}
 
-        scn_half = slow;
-        prev_slow->next = NULL;
-        reverse(&scn_half);
-        isp = compare(*head, scn_half);
+	return (1);
+}
 
-        if (middle != NULL)
-        {
-            prev_slow->next = middle;
-            middle->next = scn_half;
-        }
-        else
-        {
-            prev_slow->next = scn_half;
-        }
-    }
+/**
+ * reverse_list - Reverse a linked list
+ * @head: The list
+ *
+ * Return: Pointer to the new head
+ */
+listint_t *reverse_list(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
-    return (isp);
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+	return (*head);
 }
