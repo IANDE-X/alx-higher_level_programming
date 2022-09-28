f9eb89e3f3daf314c5478c5c112b234b4cfc7cae
The Ian <74612008+IANDE-X@users.noreply.github.com>
2022-09-28 13:24:21 +0200

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import hidden_4
-
-    names = dir(hidden_4)
-
-    for name in names:
-        if "__" not in name[:2]:
-            print("{:s}".format(name))
+    all_dir = dir(hidden_4)
+    for i in range(0, len(all_dir)):
+        if "__" != all_dir[i][:2]:
+            print(all_dir[i])
