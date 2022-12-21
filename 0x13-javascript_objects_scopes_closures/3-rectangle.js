2d8b13cb8ef832f3b64336d114f66559ac664599
The Ian <74612008+IANDE-X@users.noreply.github.com>
2022-12-21 20:00:31 +0100

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-class Rectangle {
+
+module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
@@ -12,5 +13,4 @@ class Rectangle {
       console.log('X'.repeat(this.width));
     }
   }
-}
-module.exports = Rectangle;
+};
